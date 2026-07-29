77b085b7df7e827aba55ad1129b3691ae1d39388
aboucault <aur211@live.fr>
2026-07-29 14:35:05 +0200

    Ajoute la config Sanity Studio (types existants, structureTool + visionTool)

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'u6nnkwb0',
+    dataset: 'production',
+  },
+})
---
+++ sanity.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schema'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Revues Vintage',
+  projectId: 'u6nnkwb0',
+  dataset: 'production',
+  plugins: [structureTool(), visionTool()],
+  schema: {
+    types: schemaTypes,
+  },
+})
